cdabea8f7862f732efa5e261fe138b270be7b0ee
Thomas Vargiu <thomas.vargiu@facile.it>
2020-04-05 19:27:11 +0200

    Custom token validate class

--- src/Validate/Validate.php
+++ src/Validate/Validate.php
@@ -4,19 +4,68 @@ declare(strict_types=1);
 
 namespace Facile\JoseVerifier\Validate;
 
-use function array_merge;
-use Jose\Component\Signature\Algorithm\None;
+use Facile\JoseVerifier\Exception\RuntimeException;
+use Jose\Component\Checker;
+use Jose\Component\Core\AlgorithmManager;
+use Jose\Component\Core\Util\JsonConverter;
+use Jose\Component\Signature\Algorithm;
+use Jose\Component\Signature\JWSTokenSupport;
+use Jose\Component\Signature\JWSVerifier;
+use Jose\Component\Signature\Serializer\CompactSerializer;
+use Jose\Easy\AbstractLoader;
+use Jose\Easy\JWT;
 
-class Validate extends \Jose\Easy\Validate
+class Validate extends AbstractLoader
 {
+    public static function token(string $token): self
+    {
+        return new self($token);
+    }
+
+    public function run(): JWT
+    {
+        if (0 !== \count($this->allowedAlgorithms)) {
+            $this->headerCheckers[] = new Checker\AlgorithmChecker($this->allowedAlgorithms, true);
+        }
+        $jws = (new CompactSerializer())->unserialize($this->token);
+        $headerChecker = new Checker\HeaderCheckerManager($this->headerCheckers, [new JWSTokenSupport()]);
+        $headerChecker->check($jws, 0);
+
+        $verifier = new JWSVerifier(new AlgorithmManager($this->algorithms));
+        if (! $verifier->verifyWithKeySet($jws, $this->jwkset, 0)) {
+            throw new RuntimeException('Invalid signature');
+        }
+
+        $jwt = new JWT();
+        $jwt->header->replace($jws->getSignature(0)->getProtectedHeader());
+        $jwt->claims->replace(JsonConverter::decode($jws->getPayload() ?? '{}'));
+
+        $claimChecker = new Checker\ClaimCheckerManager($this->claimCheckers);
+        $claimChecker->check($jwt->claims->all(), $this->mandatoryClaims);
+
+        return $jwt;
+    }
+
     /**
      * @return string[]
      */
     protected function getAlgorithmMap(): array
     {
-        return array_merge(
-            [None::class],
-            parent::getAlgorithmMap()
-        );
+        return [
+            Algorithm\None::class,
+            Algorithm\HS256::class,
+            Algorithm\HS384::class,
+            Algorithm\HS512::class,
+            Algorithm\RS256::class,
+            Algorithm\RS384::class,
+            Algorithm\RS512::class,
+            Algorithm\PS256::class,
+            Algorithm\PS384::class,
+            Algorithm\PS512::class,
+            Algorithm\ES256::class,
+            Algorithm\ES384::class,
+            Algorithm\ES512::class,
+            Algorithm\EdDSA::class,
+        ];
     }
 }
